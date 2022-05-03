acadee239756f6d2837d368c7baf8a5389732968
Mitali Avhad <mitali4business@gmail.com>
2022-05-03 14:33:53 +0530

    Add files via upload

---
+++ 01. Recursion/Nested recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//As the recursive function is passed in the parameter of a recursive function call it is nested recursion.
+int fun(int n)
+{
+    if(n>100)
+        return n-10;
+    return fun(fun(n+11));
+}
+
+int main()
+{
+    int r;
+    //Any value entered in the parameters below 100 will give 91 as output because the tracing tree
+    //will bring the function to fun(101) and then 101-10=91
+    r=fun(95);
+    cout<<r;
+    return 0;
+}
