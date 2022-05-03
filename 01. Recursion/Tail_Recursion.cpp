9ea8da50bd151ed5bf4f5a4e39420405e538a32d
Mitali Avhad <mitali4business@gmail.com>
2022-05-03 14:13:47 +0530

    Create Tail_Recursion.cpp

---
+++ 01. Recursion/Tail_Recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//As the recursive function call is the last statement in the if block it is a type of tail recursion.
+void fun(int n)
+{
+    if(n>0)
+    {
+        cout<<n<<" ";
+        fun(n-1);
+    }
+} //prints 3 2 1
+
+int main()
+{
+    int x=3;
+    fun(x);
+    // fun1(x);
+    return 0;
+}
